4e9c333ac541568cd6342460ae37ef297bf1ca78
Edipo Elwes <edipoelwes2@gmail.com>
2019-10-20 23:14:43 -0300

    Alterando configuraçao de DEBUG para False

--- GestaoDeEstoque/settings.py
+++ GestaoDeEstoque/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't9tn-dn19uq$%k5_($*orko^1e@*r0g#bz80&gupx*883@s605'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -74,12 +74,6 @@ WSGI_APPLICATION = 'GestaoDeEstoque.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
     'default': {
@@ -91,6 +85,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'littleone',
+#         'USER': 'root',
+#         'PASSWORD': 'Ed4354Po',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
